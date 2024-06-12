0304ce239ac449c8b7b7af6778d0f57813fa8945
ciraganenicole <ciraganenicole@gmail.com>
2024-06-12 15:25:35 +0200

    fix

--- src/utils/team.ts
+++ src/utils/team.ts
@@ -8,22 +8,22 @@ export const team = [
     description:
       'Passionate about Blockchain and Cardano. Worked with several companies and learned a lot. So happy to share my knowlege with my fellow devs. ',
   },
-  {
-    name: 'Team member',
-    role: 'Manager',
-    icon: '/assets/images/home/why.jpg',
-    phone: '+256 890987654',
-    mail: 'directeur@gmail.com',
-    description:
-      'Passionate about Blockchain and Cardano. Worked with several companies and learned a lot. So happy to share my knowlege with my fellow devs. ',
-  },
-  {
-    name: 'Team member',
-    role: 'Secretaire',
-    icon: '/assets/images/home/why.jpg',
-    phone: '+256 890987654',
-    mail: 'directeur@gmail.com',
-    description:
-      'Passionate about Blockchain and Cardano. Worked with several companies and learned a lot. So happy to share my knowlege with my fellow devs. ',
-  },
+  // {
+  //   name: 'Team member',
+  //   role: 'Manager',
+  //   icon: '/assets/images/home/why.jpg',
+  //   phone: '+256 890987654',
+  //   mail: 'directeur@gmail.com',
+  //   description:
+  //     'Passionate about Blockchain and Cardano. Worked with several companies and learned a lot. So happy to share my knowlege with my fellow devs. ',
+  // },
+  // {
+  //   name: 'Team member',
+  //   role: 'Secretaire',
+  //   icon: '/assets/images/home/why.jpg',
+  //   phone: '+256 890987654',
+  //   mail: 'directeur@gmail.com',
+  //   description:
+  //     'Passionate about Blockchain and Cardano. Worked with several companies and learned a lot. So happy to share my knowlege with my fellow devs. ',
+  // },
 ];
