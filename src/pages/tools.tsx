81bbb81218e877fc23987fbbe0bd31d06e0fda91
ciraganenicole <ciraganenicole@gmail.com>
2024-06-12 15:14:41 +0200

    Fixes

--- src/pages/tools.tsx
+++ src/pages/tools.tsx
@@ -36,25 +36,25 @@ const Tools = () => {
       </div>
 
       <div className="px-4 py-8 md:px-16">
-        <div className="mt-12 grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-16">
+        <div className="mt-12 grid grid-cols-1 gap-8 md:grid-cols-3 md:gap-16">
           {tools.map((tool, index) => {
             return (
               <div
                 key={index}
-                className="flex h-[80vh] flex-col rounded-md bg-white text-black shadow-md"
+                className="flex h-[400px] flex-col rounded-md bg-white text-black shadow-md md:h-[80vh]"
               >
                 <img
                   src={tool.image}
                   alt={`${tool.name} Logo`}
-                  className="h-[40%] w-full object-cover md:h-[60%]"
+                  className="h-[70%] w-full object-cover md:h-[60%]"
                 />
-                <div className="flex flex-col gap-6 p-2 md:p-6">
+                <div className="flex flex-col gap-2 p-2 md:gap-6 md:p-6">
                   <h1 className="text-[14px] font-bold text-primary-700 md:text-xl">
                     {tool.name}
                   </h1>
                   <p className="text-[11px] md:text-sm">{tool.description}</p>
                   <Link href={`/tools/${tool.link}`}>
-                    <button className="w-[60%] rounded-sm bg-primary-700 p-1 text-[11px] text-white md:w-[50%] md:text-sm">
+                    <button className="w-[40%] rounded-sm bg-primary-700 p-1 text-[11px] text-white md:text-sm">
                       Voir les details
                     </button>
                   </Link>
